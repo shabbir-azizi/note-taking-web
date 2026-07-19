b2a0ea64ce7069019e47d6a174fb2e9a42a37031
unknown <azizishabbir355@gmail.com>
2026-07-19 14:35:59 +0500

    making new project about note website

--- website/auth.py
+++ website/auth.py
@@ -1,3 +1,15 @@
 from flask import Blueprint
 
 views = Blueprint('auth',__name__)
+
+@auth.route('/login')
+def login():
+    return "<p> Login<p>"
+
+@auth.route('/logout')
+def logout():
+    return"<p>logout</p>"
+
+@auth.route('/sign-up')
+def sign_up():
+    return "<p>Sign Up</p>"
